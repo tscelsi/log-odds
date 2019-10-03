f023f0841e92e737cd342cdc97636966642ffbb6
Sean Miller <seanbeyrau@gmail.com>
2019-10-03 16:09:04 -0400

    updates

--- README.md
+++ README.md
@@ -1,20 +1,29 @@
 # log-odds
-This is a Log-odds, informative Dirichlet prior method corpora comparison.
-Running the script will produce two files, with the top 50 ranked log-odds ratios for each corpus.
+This is a log-odds, informative Dirichlet prior method for corpora comparison as 
+presented in:<br><br>
+Monroe, B. L., Colaresi, M. P., and Quinn, K. M. 2008.  Fightin' words: lexical
+feature selection and evaluation for identifying the content of political
+conflict. Political Analysis 16(4): 372-40.
+
+To run this script, you should first generate or get a hold of three tsv files,
+i.e., two corpora for comparison and a third corpus for prior token frequency 
+estimation. The tsv's should have one token and the its frequency count on each 
+line.
 
 Usage
 =======
 ```
 usage: log_odds.py [-h] --corpus_1 CORPUS_1 --corpus_2 CORPUS_2 
-		   --corpus_bg CORPUS_BG --prefix PREFIX [--raw]
+		   --corpus_bg CORPUS_BG --lim [--out] [--raw]
 
 optional arguments:
   -h, --help            show this help message and exit
-  --corpus_1 CORPUS_1   target corpus for ranking
-  --corpus_2 CORPUS_2   target corpus for comparison
-  --corpus_bg CORPUS_BG
-                        backgroud corpus
-  --prefix PREFIX       prefix for ranked tsv outputs
-  --raw                 flag to use unscaled log-odds-ratios
+  corpus_1 CORPUS_1     first corpus, a tsv of token + counts
+  corpus_2 CORPUS_2     second corpus tsv
+  corpus_bg CORPUS_BG
+                        backgroud corpus tsv
+  --lim			number of ranked tokens for comparison
+  --out		        optional path for ranked output
+  --raw                 flag to use unscaled log-odds ratios
 ```
 
--- log_odds.py
+++ log_odds.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
-""" Calculate token log-odds-ratios using frequency distributions of two corpora and 
+""" Calculate token log-odds ratios using frequency distributions of two corpora using 
     a background corpus. Defaults to z-score scaling unless '--raw' flag is used. """
 
 import argparse
 import logging
 import math
+import operator
 from typing import Iterator
 
 
@@ -25,75 +26,89 @@ def _size(corpus: dict) -> int:
     return sum(corpus.values())
 
 
-def log_odds(word: str, c1: dict, c2: dict, bg: dict, scale: bool) -> float:
-    if word in c1 and word in c2 and word in bg:
-        numerator_1 = c1[word] + bg[word]
-        numerator_2 = c2[word] + bg[word]
-        denom_1 = _size(c1) + _size(bg) - numerator_1
-        denom_2 = _size(c2) + _size(bg) - numerator_2
-        raw_logodds = math.log(numerator_1 / denom_1) - math.log(
-            numerator_2 / denom_2
-        )
+def _log_odds(
+    word: str,
+    c1: dict,
+    c2: dict,
+    bg: dict,
+    size1: int,
+    size2: int,
+    size3: int,
+    raw: bool,
+) -> float:
+    numerator_1 = c1[word] + bg[word]
+    numerator_2 = c2[word] + bg[word]
+    denom_1 = size1 + size3 - numerator_1
+    denom_2 = size2 + size3 - numerator_2
+    raw_logodds = math.log(numerator_1 / denom_1) - math.log(
+        numerator_2 / denom_2
+    )
+    if raw:
+        return raw_logodds
+    else:
         variance = (1 / numerator_1) + (1 / numerator_2)
-        if scale:
-            return raw_logodds / math.sqrt(variance)
-        else:
-            return raw_logodds
+        return raw_logodds / math.sqrt(variance)
 
 
 def main(args: argparse.Namespace) -> None:
 
-    freq_1 = _get_dict(args.corpus_1)
-    freq_2 = _get_dict(args.corpus_2)
-    freq_bg = _get_dict(args.corpus_bg)
+    c_1 = _get_dict(args.corpus_1)
+    c_2 = _get_dict(args.corpus_2)
+    c_bg = _get_dict(args.corpus_bg)
+
+    size1 = _size(c_1)
+    size2 = _size(c_2)
+    size3 = _size(c_bg)
+
+    supported_tokens = set(c_1.keys())
+    supported_tokens &= c_2.keys()
+    supported_tokens &= c_bg.keys()
 
     if args.raw:
-        logging.info("Calculating token log-odds-ratios scaled by z-score.")
-    else:
         logging.info("Calculating token log-odds-ratios.")
+    else:
+        logging.info("Calculating token log-odds-ratios scaled by z-score.")
 
     ratios = []
-    for tok in freq_1:
-        rat = log_odds(tok, freq_1, freq_2, freq_bg, args.raw)
-        if rat:
-            ratios.append((tok, rat))
+    for tok in supported_tokens:
+        rat = _log_odds(tok, c_1, c_2, c_bg, size1, size2, size3, args.raw)
+        ratios.append((tok, rat))
 
-    ratios.sort(key=lambda x: x[1], reverse=True)
+    ratios.sort(key=operator.itemgetter(1), reverse=True)
 
-    with open(f"{args.prefix}_top50.tsv", "w") as sink:
-        for tok, rat in ratios[:50]:
-            sink.write(f"{tok}\t{rat}\n")
-
-    logging.info(
-        "Top 50 tokens from %s written to %s", args.corpus_1, sink.name
-    )
-
-    with open(f"{args.prefix}_btm50.tsv", "w") as sink:
-        for tok, rat in sorted(ratios, key=lambda x: x[1])[:50]:
-            sink.write(f"{tok}\t{rat}\n")
-
-    logging.info(
-        "Bottom 50 tokens from %s written to %s", args.corpus_2, sink.name
-    )
+    if args.out:
+        with open(f"{args.out}", "w") as sink:
+            sink.write(f"Top {args.lim} from {args.corpus_1}\n")
+            for tok, rat in ratios[: args.lim]:
+                sink.write(f"{tok}\t{rat}\n")
+            sink.write(f"Top {args.lim} from {args.corpus_2}\n")
+            for tok, rat in ratios[: -args.lim - 1 : -1]:
+                sink.write(f"{tok}\t{rat}\n")
+    else:
+        print(f"Top {args.lim} from {args.corpus_1}")
+        for tok, rat in ratios[: args.lim]:
+            print(f"{tok}\t{rat}")
+        print(f"\nTop {args.lim} from {args.corpus_2}")
+        for tok, rat in ratios[: -args.lim - 1 : -1]:
+            print(f"{tok}\t{rat}")
 
 
 if __name__ == "__main__":
     logging.basicConfig(level="INFO", format="%(levelname)s: %(message)s")
     parser = argparse.ArgumentParser()
     parser.add_argument(
-        "--corpus_1", required=True, help="target corpus for ranking"
+        "corpus_1", help="first corpus, a tsv of token + counts"
     )
+    parser.add_argument("corpus_2", help="second corpus tsv for comparison")
+    parser.add_argument("corpus_bg", help="backgroud corpus tsv")
+    parser.add_argument("--lim", type=int, help="Num of top ranked words")
     parser.add_argument(
-        "--corpus_2", required=True, help="target corpus for comparison"
-    )
-    parser.add_argument("--corpus_bg", required=True, help="backgroud corpus")
-    parser.add_argument(
-        "--prefix", required=True, help="prefix for ranked tsv outputs"
+        "--out", default=False, help="optional path to ranked output file"
     )
     parser.add_argument(
         "--raw",
-        default=True,
-        action="store_false",
+        default=False,
+        action="store_true",
         help="flag to use unscaled log-odds-ratios",
     )
     main(parser.parse_args())
